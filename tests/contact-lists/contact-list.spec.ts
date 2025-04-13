1cf129f251aff39bcaa4f74d9c686a6d5d81c9e3
Cyril Arickathil <cyriljohn21@gmail.com>
2025-04-14 00:10:10 +0530

    added schema validation

--- tests/contact-lists/contact-list.spec.ts
+++ tests/contact-lists/contact-list.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-
+import {z} from 'zod'
 import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
@@ -19,6 +19,25 @@ test("GET [2]/contacts lists", async ({ request }) => {
   //path.resolve(__dirname, "../../.env");
 
   fs.writeFileSync(envPath, `CONTACT_ID=${body[0]._id}\n`);
+ await expect(response).toBeOK();
+  const schema = z.array(
+    z.object(
+      {
+         _id: z.string(),
+      firstName: z.number(),
+      lastName: z.string(),
+    email: z.string(),
+    owner: z.string(),
+    __v: z.number(),
+      }
+    )
+     
+  )
+  expect(() =>
+  {
+schema.safeParse(body)
+  }).not.toThrow()
+  
 });
 
 test("GET [3]/contacts/1", async ({ request }) => {
